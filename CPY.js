f81860b86f36aea722e65b331659ee5afae85492
mosami789 <84531898+mosami789@users.noreply.github.com>
2021-07-21 12:37:02 +0200

    Create CPY.js

---
+++ CPY.js
@@ -0,0 +1,25 @@
+<!-- 
+var message=""; 
+function clickIE() {if (document.all) {(message);return false;}} 
+function clickNS(e) {if 
+(document.layers||(document.getElementById&&!document.all)) { 
+if (e.which==2||e.which==3) {(message);return false;}}} 
+if (document.layers) 
+{document.captureEvents(Event.MOUSEDOWN);document.onmousedown=clickNS;} 
+else{document.onmouseup=clickNS;document.oncontextmenu=clickIE;} 
+document.oncontextmenu=new Function("return false") 
+// --> 
+</script> 
+<script language="JavaScript1.2"> 
+function disabletextselect(i){ 
+return false 
+} 
+function renabletextselect(){ 
+return true 
+} 
+//if IE4+ 
+document.onselectstart=new Function ("return false") 
+//if NS6+ 
+if (window.sidebar){ 
+document.onmousedown=disabletextselect 
+document.onclick=renabletextselect 
